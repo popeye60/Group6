be481a0f04f2c965ead6893097b679d2eb977d3d
popeye60 <Gaidmanee@gmail.com>
2020-05-19 15:56:19 +0700

    sudmit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link href="assets\css\login.css" rel="stylesheet" type="text/css">
+</head>
+<form>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <h1>Login</h1>
+            </div>
+            <!-- Login Form -->
+            <form>
+                <input type="text" id="name" class="fadeIn second" name="login" placeholder="Username">
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+                <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+                <a class="underlineHover" href="#">Forgot Password?</a>
+            </div>
+        </div>
+    </div>
+
+</html>
